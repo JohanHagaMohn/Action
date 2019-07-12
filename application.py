ab165c307a321a824eeca867c89fc0a58d583868
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2019-07-12 02:51:48 +0200

    Configured Flask and setup template index page

--- application.py
+++ application.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-db = SQLAlchemy()
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html"), 200
