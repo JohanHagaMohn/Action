c54c63377d16b651fd886de838104b74408f9e81
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2019-07-16 02:04:46 +0200

    Added logo, favicon, and disabled response caching

--- application.py
+++ application.py
@@ -25,3 +25,11 @@ def create_app():
 
 
 app = create_app()
+
+
+@app.after_request
+def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
---
+++ static/script.js
@@ -0,0 +1,5 @@
+function openMenu(id) {
+    alert(id);
+}
+
+window.addEventListener("DOMContentLoaded", () => {});
