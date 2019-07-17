a5ad48048cb7d60b1df4afb4ebf82098715637a5
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2019-07-17 04:28:21 +0200

    Added night mode setting

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ app = create_app()
 
 @app.after_request
 def after_request(response):
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        response.headers["Expires"] = 0
-        response.headers["Pragma"] = "no-cache"
-        return response
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
--- static/script.js
+++ static/script.js
@@ -1,5 +1,78 @@
-function openMenu(id) {
-    alert(id);
-}
+window.addEventListener("DOMContentLoaded", () => {
+    var menuClicked = false;
+    var bodyColor = document.body.style.backgroundColor;
+    function showMenu(opacity) {
+        function change(item) {
+            item.opacity = 1 - opacity;
+            if (opacity == 0) {
+                setTimeout(() => {
+                    item.visibility = "none";
+                    item.display = "none";
+                }, 150);
+                item.pointerEvents = "none";
+                item.opacity = opacity;
+            } else {
+                item.cursor = "pointer";
+                item.pointerEvents = "all";
+                item.visibility = "visible";
+                item.display = "flex";
+                setTimeout(() => {
+                    item.opacity = opacity;
+                }, 5);
+            }
+        }
+        document.querySelectorAll(".container").forEach(element => {
+            change(element.style);
+        });
+        document.querySelectorAll(".label").forEach(element => {
+            change(element.style);
+        });
+        const light = bodyColor == "#fff" ? "moon" : "sun";
+        const icon = document.querySelector(`#${light}`).style;
+        change(icon);
+    }
+    if (!bodyColor) {
+        bodyColor = "#fff";
+    }
+    document.querySelector("main").addEventListener("click", () => {
+        if (menuClicked) {
+            menuClicked = false;
+            showMenu(0);
+        }
+    });
+    document.querySelector(".container").addEventListener("click", () => {
+        showMenu(0);
+        if (bodyColor == "#fff") {
+            bodyColor = "#000";
+            document.querySelector("body").style.backgroundColor = "#000";
+            document.querySelector("header").style.boxShadow =
+                "0 2px 2px -2px rgba(255, 255, 255, 0.5)";
+            document.querySelector(".container").style.boxShadow =
+                "-2px 2px 2px -2px rgba(255, 255, 255, 0.5)";
+            document.querySelector(".container").style.outline =
+                "1px solid rgba(50, 50, 50, 0.25)";
+            document.querySelector("#bars").style.color = "#777";
+            document.querySelector("#bars").style.opacity = "0.5";
+        } else {
+            bodyColor = "#fff";
+            document.querySelector("body").style.backgroundColor = "#fff";
+            document.querySelector("header").style.boxShadow =
+                "0 2px 2px -2px rgba(0, 0, 0, 0.5)";
+            document.querySelector(".container").style.boxShadow =
+                "-2px 2px 2px -2px rgba(0, 0, 0, 0.5)";
+        }
+        showMenu(0.5);
+    });
 
-window.addEventListener("DOMContentLoaded", () => {});
+    document.getElementById("settingicon").addEventListener("click", () => {
+        menuClicked = !menuClicked;
+        if (bodyColor == "") {
+            document.body.style.backgroundColor = "#fff";
+        }
+        if (menuClicked) {
+            showMenu(1);
+        } else {
+            showMenu(0);
+        }
+    });
+});
