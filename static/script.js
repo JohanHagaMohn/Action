db794f931504db7cd98d2d8255936206e60de092
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2019-07-18 03:35:19 +0200

    Finalized toggleable dark mode menu and media queries

--- static/script.js
+++ static/script.js
@@ -1,74 +1,104 @@
+function eventFire(el, etype) {
+    /** Simulates event, from stackoverflow.com/questions/2705583 */
+    if (el.fireEvent) {
+        el.fireEvent("on" + etype);
+    } else {
+        var evObj = document.createEvent("MouseEvents");
+        evObj.initEvent(etype, true, false);
+        !el.dispatchEvent(evObj);
+    }
+}
+function hideIcon(light, opacity) {
+    /** Changes night mode icon and label based on opacity */
+    if (opacity == 1) {
+        document.querySelector(`#${light}`).style.display = "none";
+    } else {
+        document.querySelector(`#${light}`).style.display = "flex";
+    }
+    label = document.querySelector("#nightLabel");
+    if (light == "moon") {
+        label.innerHTML = "Light Mode";
+    } else {
+        label.innerHTML = "Dark Mode";
+    }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
+    /** Declares header variables */
     var menuClicked = false;
+    var smallMenu = false;
     var bodyColor = document.body.style.backgroundColor;
-    function showMenu(opacity) {
-        function change(item) {
-            item.opacity = 1 - opacity;
+    function showContainers(opacity) {
+        /** Toggles menu items on small screens*/
+        if (window.innerWidth < 540 && smallMenu == false && opacity == 1) {
+            document.querySelector("#about").style.display = "initial";
+            document.querySelector("#register").style.display = "initial";
+            document.querySelector("#login").style.display = "initial";
+            smallMenu = true;
+        } else if (smallMenu && window.innerWidth < 540) {
+            document.querySelector("#about").style.display = "none";
+            document.querySelector("#register").style.display = "none";
+            document.querySelector("#login").style.display = "none";
+            smallMenu = false;
+        }
+
+        /** Changes containers' visibility based on opacity */
+        document.querySelectorAll(".container").forEach(element => {
             if (opacity == 0) {
                 setTimeout(() => {
-                    item.visibility = "none";
-                    item.display = "none";
-                }, 150);
-                item.pointerEvents = "none";
-                item.opacity = opacity;
+                    element.style.visibility = "none";
+                }, 100);
+                element.style.opacity = opacity;
+                element.style.display = "none";
+
+                /** Simulates click event to reset event listeners */
+                eventFire(document.querySelector("main"), "click");
             } else {
-                item.cursor = "pointer";
-                item.pointerEvents = "all";
-                item.visibility = "visible";
-                item.display = "flex";
+                element.style.visibility = "visible";
+                element.style.display = "flex";
                 setTimeout(() => {
-                    item.opacity = opacity;
-                }, 5);
+                    element.style.opacity = opacity;
+                }, 100);
             }
-        }
-        document.querySelectorAll(".container").forEach(element => {
-            change(element.style);
         });
-        document.querySelectorAll(".label").forEach(element => {
-            change(element.style);
-        });
-        const light = bodyColor == "#fff" ? "moon" : "sun";
-        const icon = document.querySelector(`#${light}`).style;
-        change(icon);
+    }
+    function showMenu(opacity) {
+        /** Hides containers and svg */
+        hideIcon(bodyColor == "#fff" ? "sun" : "moon", opacity);
+        showContainers(opacity);
     }
     if (!bodyColor) {
+        /** Sets default bodyColor white */
         bodyColor = "#fff";
     }
     document.querySelector("main").addEventListener("click", () => {
         if (menuClicked) {
-            menuClicked = false;
+            /** Deselects menu on main click */
+            menuClicked = !menuClicked;
             showMenu(0);
         }
     });
-    document.querySelector(".container").addEventListener("click", () => {
+    document.querySelector("#nightMode").addEventListener("click", () => {
+        /** Deselects menu and changes styling on nightMode click */
         showMenu(0);
+        document.querySelector("main").style.color = bodyColor;
         if (bodyColor == "#fff") {
             bodyColor = "#000";
-            document.querySelector("body").style.backgroundColor = "#000";
             document.querySelector("header").style.boxShadow =
-                "0 2px 2px -2px rgba(255, 255, 255, 0.5)";
-            document.querySelector(".container").style.boxShadow =
-                "-2px 2px 2px -2px rgba(255, 255, 255, 0.5)";
-            document.querySelector(".container").style.outline =
-                "1px solid rgba(50, 50, 50, 0.25)";
-            document.querySelector("#bars").style.color = "#777";
-            document.querySelector("#bars").style.opacity = "0.5";
+                "0 2px 2px -2px rgba(200, 200, 200, 0.5)";
         } else {
             bodyColor = "#fff";
-            document.querySelector("body").style.backgroundColor = "#fff";
             document.querySelector("header").style.boxShadow =
                 "0 2px 2px -2px rgba(0, 0, 0, 0.5)";
-            document.querySelector(".container").style.boxShadow =
-                "-2px 2px 2px -2px rgba(0, 0, 0, 0.5)";
         }
-        showMenu(0.5);
+        document.querySelector("body").style.backgroundColor = bodyColor;
+        document.querySelectorAll(".container").forEach(element => {
+            element.style.backgroundColor = bodyColor;
+        });
     });
-
     document.getElementById("settingicon").addEventListener("click", () => {
+        /** Inverts menuClicked boolean and displays menu accordingly */
         menuClicked = !menuClicked;
-        if (bodyColor == "") {
-            document.body.style.backgroundColor = "#fff";
-        }
         if (menuClicked) {
             showMenu(1);
         } else {
